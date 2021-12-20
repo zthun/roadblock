2ee84d54224a81ebe5d26ccb7b2db9d6591d0464
Anthony Bonta <zthun.coding@gmail.com>
2021-12-20 14:52:19 -0600

    feat: add the base layout

--- packages/roadblock.web/src/index.tsx
+++ packages/roadblock.web/src/index.tsx
@@ -1,6 +1,17 @@
+import { ZTopNavAvatar, ZWebAppLayout } from '@zthun/works.react';
 import React from 'react';
 import { render } from 'react-dom';
-import { ZRoadblockApp } from './app/app';
-import './index.less';
+import { Route } from 'react-router-dom';
+import '../images/svg/zthunworks-owl.svg';
+import { ZLoginPage } from './login/login-page';
+import { ZProfilePage } from './profile/profile-page';
 
-render(<ZRoadblockApp />, document.getElementById('zthunworks'));
+const ZAvatarOwl = <ZTopNavAvatar src='images/svg/zthunworks-owl.svg' />;
+
+render(
+  <ZWebAppLayout headerText='Zthunworks' whoami='roadblock' home='profile' profileApp='roadblock' avatar={ZAvatarOwl}>
+    <Route exact path='/login' component={ZLoginPage} />
+    <Route exact path='/profile' component={ZProfilePage} />
+  </ZWebAppLayout>,
+  document.getElementById('zthunworks')
+);
