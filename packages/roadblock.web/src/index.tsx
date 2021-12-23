f5ea8799a33efe589a147ed9ebd196f40ff202da
Anthony Bonta <zthun.coding@gmail.com>
2021-12-22 21:19:05 -0600

    refactor: updated to new layout api

--- packages/roadblock.web/src/index.tsx
+++ packages/roadblock.web/src/index.tsx
@@ -1,4 +1,4 @@
-import { ZTopNavAvatar, ZWebAppLayout } from '@zthun/works.react';
+import { ZWebAppLayout } from '@zthun/works.react';
 import React from 'react';
 import { render } from 'react-dom';
 import { Route } from 'react-router-dom';
@@ -6,10 +6,8 @@ import '../images/svg/zthunworks-owl.svg';
 import { ZLoginPage } from './login/login-page';
 import { ZProfilePage } from './profile/profile-page';
 
-const ZAvatarOwl = <ZTopNavAvatar src='images/svg/zthunworks-owl.svg' />;
-
 render(
-  <ZWebAppLayout headerText='Zthunworks' whoami='roadblock' home='profile' profileApp='roadblock' avatar={ZAvatarOwl}>
+  <ZWebAppLayout whoami='roadblock' home='profile' profileApp='roadblock'>
     <Route exact path='/login' component={ZLoginPage} />
     <Route exact path='/profile' component={ZProfilePage} />
   </ZWebAppLayout>,
