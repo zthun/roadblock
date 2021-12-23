c6fb6ba0662cf6eff249e4bf9c54ccc5b5f3b2f5
Anthony Bonta <zthun.coding@gmail.com>
2021-12-22 22:47:40 -0600

    style: fixing lint errors

--- lint-janitor.config.js
+++ lint-janitor.config.js
@@ -1,30 +1,25 @@
-const generated = ['**/CHANGELOG.md', 'packages/**/dist/**', 'packages/**/docs/**', 'node_modules/**', 'packages/**/node_modules/**'];
-const k8sTemplates = ['packages/works.k8s/templates/**'];
+const generated = ['**/CHANGELOG.md', 'packages/**/dist/**', 'packages/**/docs/**', 'node_modules/**', 'packages/**/node_modules/**', '.yarnrc.yml'];
 const partialGenerated = ['cspell.json', 'lerna.json'];
 
 const esFiles = ['*.js', '*.ts', 'packages/**/src/**/*.ts', 'packages/**/src/**/*.tsx'];
-const styleFiles = ['packages/**/src/**/*.less', 'packages/**/less/**/*.less'];
 const htmlFiles = ['packages/**/src/**/*.html'];
 const markdownFiles = ['*.md', 'packages/**/*.md'];
 const jsonFiles = ['*.json', 'packages/**/*.json'];
 const yamlFiles = ['*.yaml', '*.yml', 'packages/**/*.yaml', 'packages/**/*.yml'];
-const prettyFiles = [].concat(esFiles).concat(styleFiles).concat(htmlFiles).concat(markdownFiles).concat(jsonFiles).concat(yamlFiles);
-const spellingFiles = [].concat(esFiles).concat(styleFiles).concat(htmlFiles).concat(markdownFiles).concat(jsonFiles).concat(yamlFiles);
+const prettyFiles = [].concat(esFiles).concat(htmlFiles).concat(markdownFiles).concat(jsonFiles).concat(yamlFiles);
+const spellingFiles = [].concat(esFiles).concat(htmlFiles).concat(markdownFiles).concat(jsonFiles).concat(yamlFiles);
 
 const esFilesExclude = generated;
-const styleFilesExclude = generated;
 const htmlFilesExclude = generated;
 const markdownFilesExclude = generated;
 const jsonFilesExclude = generated;
-const yamlFilesExclude = generated.concat(k8sTemplates);
-const prettyFilesExclude = generated.concat(k8sTemplates).concat(partialGenerated);
-const spellingFilesExclude = generated.concat(k8sTemplates).concat(partialGenerated);
+const yamlFilesExclude = generated;
+const prettyFilesExclude = generated.concat(partialGenerated);
+const spellingFilesExclude = generated.concat(partialGenerated);
 
 module.exports = {
   esFiles,
   esFilesExclude,
-  styleFiles,
-  styleFilesExclude,
   htmlFiles,
   htmlFilesExclude,
   markdownFiles,
