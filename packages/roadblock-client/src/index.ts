e1f072884dbbdea3c4ecab611c89bbbd30e1da7f
Anthony Bonta <zthun.coding@gmail.com>
2023-11-14 09:11:59 -0600

    feat: client adds support to invoke users and cookies microservice

---
+++ packages/roadblock-client/src/index.ts
@@ -0,0 +1,13 @@
+/* istanbul ignore file */
+// Apps
+export { ZAppsClient } from './apps/apps.client';
+export { ZAppsModule } from './apps/apps.module';
+// Cookies
+export { ZCookiesClient } from './cookies/cookies.client';
+export { ZCookiesModule } from './cookies/cookies.module';
+// Notifications
+export { ZNotificationsClient } from './notifications/notifications.client';
+export { ZNotificationsModule } from './notifications/notifications.module';
+// Users
+export { ZUsersClient } from './users/users.client';
+export { ZUsersModule } from './users/users.module';
