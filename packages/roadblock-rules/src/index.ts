25528f752fa8ddf6373844564da03b4fe7a0f27e
Anthony Bonta <zthun.coding@gmail.com>
2023-11-14 09:24:54 -0600

    feat: the rules library adds nest guards for authentication

---
+++ packages/roadblock-rules/src/index.ts
@@ -0,0 +1,11 @@
+/* istanbul ignore file */
+
+// Security
+export { ZRuleCookieRequiresAuthActivated } from './security/rule-cookie-requires-auth-activated.guard';
+export { ZRuleCookieRequiresAuthAny } from './security/rule-cookie-requires-auth-any.guard';
+export { ZRuleCookieRequiresAuthDeactivated } from './security/rule-cookie-requires-auth-deactivated.guard';
+export { ZRuleCookieRequiresAuthRegular } from './security/rule-cookie-requires-auth-regular.guard';
+export { ZRuleCookieRequiresAuth } from './security/rule-cookie-requires-auth.guard';
+export { ZSecurityController } from './security/security.controller';
+export { ZSecurityModule } from './security/security.module';
+export { ZSecurityService } from './security/security.service';
